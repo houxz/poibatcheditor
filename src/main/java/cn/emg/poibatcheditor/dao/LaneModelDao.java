aee371f5b23f3291cb44994651ede67dc38b23fe
swimmingcoach <zshang_0630@sina.com>
2019-04-24 14:27:00 +0800

    补充车道页面内容

---
+++ src/main/java/cn/emg/poibatcheditor/dao/LaneModelDao.java
@@ -0,0 +1,19 @@
+package cn.emg.poibatcheditor.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface LaneModelDao {
+	
+    List<Map<String, Object>> select(@Param("columns") Set<String> columns, @Param("code") String code, @Param("limit") Integer limit, @Param("offset") Integer offset);
+    
+    Integer count(@Param("columns") Set<String> columns, @Param("code") String code);
+    
+    List<Map<String, Object>> selectByOwners(@Param("columns") Set<String> columns, @Param("owners") Set<String> owners, @Param("limit") Integer limit, @Param("offset") Integer offset);
+    
+    Integer countByOwners(@Param("columns") Set<String> columns, @Param("owners") Set<String> owners);
+    
+}
